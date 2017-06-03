469233d5217ee287ca99c98cc0708bab17998a02
Sud0nim <Sud0nim@users.noreply.github.com>
2017-06-04 09:50:21 +1000

    Create comment_examples.nim

---
+++ comment_examples.nim
@@ -0,0 +1,19 @@
+#[ 
+You can place documentation within the comment syntax 
+and it will be ignored by the compiler.
+]#
+
+# Single line comments can be added using the hash symbol, as you would in Python code.
+
+# This is a single line comment.
+
+
+# Multi-line comments are surrounded by the hash and square bracket syntax, E.G. #[... comments here ...]#
+
+#[
+My first line of comments.
+My second line of comments.
+A third line of comments.
+This is a multi-line comment.
+]#
+
