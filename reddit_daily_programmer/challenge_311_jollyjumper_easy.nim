37f3caa3bc77edd088a174fa90317c60af33493c
Sud0nim <Sud0nim@users.noreply.github.com>
2017-06-08 20:33:36 +1000

    Update challenge_311_jollyjumper_easy.nim

--- reddit_daily_programmer/challenge_311_jollyjumper_easy.nim
+++ reddit_daily_programmer/challenge_311_jollyjumper_easy.nim
@@ -4,17 +4,19 @@
 import strutils, math
 
 proc stringsToIntegers(input: string): seq =
-  var strings = input.split()
-  var integers = newSeq[int](len(strings))
+  var 
+    strings = input.split()
+    integers = newSeq[int](len(strings))
   for i in 0..strings.high:
     integers[i] = parseInt(strings[i])
   result = integers
 
 while true:
-  var input = readline(stdin)
-  var numbers = stringsToIntegers(input)
-  var expectedSum = fac(numbers[0] - 1)
-  var actualSum = 0
+  var 
+    input = readline(stdin)
+    numbers = stringsToIntegers(input)
+    expectedSum = fac(numbers[0] - 1)
+    actualSum = 0
   for i in 1..numbers[0]:
     if i > 1:
       var diff = numbers[i] - numbers[i - 1]
@@ -23,4 +25,3 @@ while true:
     echo(numbers, " NOT JOLLY")
   else:
     echo(numbers, " JOLLY")
-
