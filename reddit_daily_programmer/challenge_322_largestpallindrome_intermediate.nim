1eb04aaa506d1a56b17a83a0ab2ba07fc99a4e28
Sud0nim <Sud0nim@users.noreply.github.com>
2017-07-06 21:43:53 +1000

    Create challenge_322_largestpallindrome_intermediate.nim

---
+++ reddit_daily_programmer/challenge_322_largestpallindrome_intermediate.nim
@@ -0,0 +1,32 @@
+
+# https://www.reddit.com/r/dailyprogrammer/comments/6ldv3m/20170705_challenge_322_intermediate_largest/
+
+import strutils except to_lower
+import unicode, math
+
+proc is_pallindrome*(text: string): bool =
+  if to_lower(text) == to_lower(reversed(text)):
+    return true
+  return false
+
+proc largest_factor*(text: string): int =
+  let number = parse_float(text)
+  int(10.pow(number)) - 1
+
+proc largest_pallindrome(text: string): int =
+  var 
+    max_factor = largest_factor(text)
+    min_factor = int(parse_float(text) / 2)
+    result, number = 0
+  for i in countdown(max_factor, min_factor):
+    for j in countdown(max_factor, min_factor):
+      number = i * j
+      if number < result:
+        break
+      if is_pallindrome($number):
+        result = number
+  return result
+
+for i in 1..8:
+  echo largest_pallindrome($i)
+
