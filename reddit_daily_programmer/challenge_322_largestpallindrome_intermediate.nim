39e6aa546eb6e64763c83b8b260abb8587838639
Sud0nim <Sud0nim@users.noreply.github.com>
2017-07-07 05:34:19 +1000

    Update challenge_322_largestpallindrome_intermediate.nim

--- reddit_daily_programmer/challenge_322_largestpallindrome_intermediate.nim
+++ reddit_daily_programmer/challenge_322_largestpallindrome_intermediate.nim
@@ -4,20 +4,20 @@
 import strutils except to_lower
 import unicode, math
 
-proc is_pallindrome*(text: string): bool =
+proc is_pallindrome(text: string): bool =
   if to_lower(text) == to_lower(reversed(text)):
     return true
   return false
 
-proc largest_factor*(text: string): int =
+proc largest_factor(text: string): int =
   let number = parse_float(text)
   int(10.pow(number)) - 1
 
 proc largest_pallindrome(text: string): int =
-  var 
+  let
     max_factor = largest_factor(text)
     min_factor = int(10.pow(parse_float(text) - 1))
-    result, number = 0
+  var result, number = 0
   for i in countdown(max_factor, min_factor):
     for j in countdown(max_factor, min_factor):
       number = i * j
